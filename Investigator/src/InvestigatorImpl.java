f4aa9f0e766827e3392bcb6e39d336ca0fae1cd5
Lior_Shmueli <shmueli@gmail.com>
2020-08-02 15:13:27 +0300

    Finish implements all Investigator interface methods

--- Investigator/src/InvestigatorImpl.java
+++ Investigator/src/InvestigatorImpl.java
@@ -1,114 +1,169 @@
 import reflection.api.Investigator;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Collections;
+import java.lang.reflect.*;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class InvestigatorImpl implements Investigator {
-    private Class theClass;
+    private Class<?> theClass;
+    private Object theInstance;
 
     @Override
     public void load(Object anInstanceOfSomething) {
-        try {
+            this.theInstance = anInstanceOfSomething;
             this.theClass = anInstanceOfSomething.getClass();
-        } catch (Exception e){
-            throw e;
-        }
     }
 
     @Override
     public int getTotalNumberOfMethods() {
-        Method[] allMethods;
-        try {
-            allMethods = theClass.getDeclaredMethods();
-        } catch (Exception e){
-            throw e;
-        }
+        Method[] allMethods = theClass.getDeclaredMethods();
         return allMethods.length;
     }
 
     @Override
     public int getTotalNumberOfConstructors() {
-        Constructor[] allConstructors;
-        try {
-             allConstructors = theClass.getDeclaredConstructors();
-        }catch (Exception e) {
-            throw e;
-        }
+        Constructor<?>[] allConstructors = theClass.getDeclaredConstructors();
         return allConstructors.length;
     }
 
     @Override
     public int getTotalNumberOfFields() {
-        Field[] allFields;
-        try {
-            allFields = theClass.getDeclaredFields();
-        }catch (Exception e){
-            throw e;
-        }
+        Field[] allFields = theClass.getDeclaredFields();
         return allFields.length;
     }
 
     @Override
     public Set<String> getAllImplementedInterfaces() {
-        Set<String> allInterfacesNames = Collections.emptySet();
-        Class[] allInterfaces;
-        allInterfaces = theClass.getInterfaces();
-        for(int i = 0; i< allInterfaces.length; i++) {
-            allInterfacesNames.add(allInterfaces[i].getName());
+        Set<String> allInterfacesNames = new HashSet<>();
+        Class<?>[] allInterfaces = theClass.getInterfaces();
+        for(Class<?> clazz: allInterfaces){
+            allInterfacesNames.add(clazz.getSimpleName());
         }
         return allInterfacesNames;
     }
 
     @Override
     public int getCountOfConstantFields() {
-        return 0;
+        int counter = 0;
+        Field[] allFields = theClass.getDeclaredFields();
+        for(Field field:allFields){
+            if(Modifier.isFinal(field.getModifiers())){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     @Override
     public int getCountOfStaticMethods() {
-        return 0;
+        int counter = 0;
+        Method[] allMethods = theClass.getDeclaredMethods();
+        for(Method method:allMethods){
+            if(Modifier.isStatic(method.getModifiers())){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     @Override
     public boolean isExtending() {
-        return false;
+        Class<?> superClass = theClass.getSuperclass();
+        return (superClass != null && superClass != Objects.class);
     }
 
     @Override
     public String getParentClassSimpleName() {
-        return null;
+        Class<?> superClass = theClass.getSuperclass();
+        if(superClass == null){
+            return null;
+        }
+        return superClass.getSimpleName();
     }
 
     @Override
     public boolean isParentClassAbstract() {
-        return false;
+        Class<?> superClass = theClass.getSuperclass();
+        if(superClass == null){
+            return false;
+        }else {
+            return Modifier.isAbstract(superClass.getModifiers());
+        }
     }
 
     @Override
     public Set<String> getNamesOfAllFieldsIncludingInheritanceChain() {
-        return null;
+        Set<String> allFieldsNames = new HashSet<>();
+        return getNamesOfAllInheritanceChainFieldsRec(allFieldsNames, this.theClass);
+    }
+
+    private Set<String> getNamesOfAllInheritanceChainFieldsRec(Set<String> fieldsNames, Class<?> superClass){
+        Field[] allFields = superClass.getDeclaredFields();
+        for(Field field:allFields){
+            fieldsNames.add(field.getName());
+        }
+
+        if(superClass.getSuperclass() != null){
+            getNamesOfAllInheritanceChainFieldsRec(fieldsNames, superClass.getSuperclass());
+        }
+        return fieldsNames;
     }
 
     @Override
-    public int invokeMethodThatReturnsInt(String methodName, Object... args) {
-        return 0;
+    public int invokeMethodThatReturnsInt(String methodName, Object... args){
+        int res = -1;
+        Method[] allMethods = theClass.getDeclaredMethods();
+        for(Method method:allMethods){
+            if(Objects.equals(method.getName(), methodName)){
+                try {
+                    res = (int) method.invoke(this.theInstance, args);
+                    break;
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    return -1;
+                }
+            }
+        }
+        return res;
     }
+    
 
     @Override
     public Object createInstance(int numberOfArgs, Object... args) {
+        for (Constructor<?> constructor : this.theClass.getDeclaredConstructors()){
+            if(constructor.getParameterCount() == numberOfArgs){
+                try {
+                    return constructor.newInstance(args);
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    return null;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public Object elevateMethodAndInvoke(String name, Class<?>[] parametersTypes, Object... args) {
-        return null;
+        Method methodToInvoke;
+        try {
+            methodToInvoke = this.theClass.getDeclaredMethod(name, parametersTypes);
+            methodToInvoke.setAccessible(true);
+            return methodToInvoke.invoke(this.theInstance, args);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            return null;
+        }
     }
 
     @Override
     public String getInheritanceChain(String delimiter) {
-        return null;
+        StringBuilder chain = new StringBuilder();
+        chain.append(theClass.getSimpleName());
+        Class<?> currentClass = theClass;
+        while(currentClass.getSuperclass() != null){
+            currentClass = currentClass.getSuperclass();
+            chain.insert(0, delimiter);
+            chain.insert(0, currentClass.getSimpleName());
+        }
+        return chain.toString();
     }
 }
